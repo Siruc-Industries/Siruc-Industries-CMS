166ed9035f948d55026bcc2f5578c11266497f21
yurysiruts <yury.siruts@gmail.com>
2025-09-10 23:48:10 +0200

    toolbar extended in quill text editor

--- pages/articles/new.vue
+++ pages/articles/new.vue
@@ -192,8 +192,18 @@ const initializeQuill = async (index: number) => {
         modules: {
           toolbar: [
             ['bold', 'italic', 'underline'],
+            ['blockquote', 'code-block'],
+            [{ 'header': 1 }, { 'header': 2 }],
             [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-            ['link', 'image'],
+            [{ 'script': 'sub'}, { 'script': 'super' }],
+            [{ 'indent': '-1'}, { 'indent': '+1' }],
+            [{ 'direction': 'rtl' }],
+            [{ 'size': ['small', false, 'large', 'huge'] }],
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            [{ 'color': [] }, { 'background': [] }],
+            [{ 'font': [] }],
+            [{ 'align': [] }],
+            ['link', 'image', 'formula'],
             ['clean']
           ]
         }
